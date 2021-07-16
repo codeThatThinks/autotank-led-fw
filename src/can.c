500409220afd9d5a785204ca4c33ebf53c80cf85
Ian Glen <ian@ianglen.me>
2021-07-16 14:53:31 -0400

    Auto recover CAN bus

--- src/can.c
+++ src/can.c
@@ -77,7 +77,7 @@ void can_init(void)
 	hcan.Init.TimeSeg1 = CAN_BS1_15TQ;
 	hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
 	hcan.Init.TimeTriggeredMode = DISABLE;
-	hcan.Init.AutoBusOff = DISABLE;
+	hcan.Init.AutoBusOff = ENABLE;
 	hcan.Init.AutoWakeUp = DISABLE;
 	hcan.Init.AutoRetransmission = DISABLE;
 	hcan.Init.ReceiveFifoLocked = DISABLE;
